4ed269133d7a3a99f60379cccbe1b1b78181e7c6
Gabriel Dicaire <gdicaire@embrionix.com>
2019-12-12 14:11:32 -0500

    syslog configuration completed

--- PrometheusMonitoring/devicemonitor/ionixmon/ionixmon/ionixmon.py
+++ PrometheusMonitoring/devicemonitor/ionixmon/ionixmon/ionixmon.py
@@ -6,7 +6,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 
-
 from flask import Flask, render_template, flash, request, send_file
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import logging
@@ -14,6 +13,7 @@ import json
 import docker
 import requests
 import re
+import time
 import os
 import datetime
 docker_client = docker.from_env()
@@ -22,12 +22,25 @@ app = Flask(__name__)
 
 class MonitoringInformation():
     TELEMETRY_URL = "/emsfp/node/v1/telemetry"
+    SYSLOG_URL = "/emsfp/node/v1/self/syslog"
     
     def __init__(self, ip, prettyName):
         self.ip = ip
         self.prettyName = prettyName
         self.prometheus_status = "NA"
         self.telemetryAvailable = self.IsTelemetryAvailable()
+        self.syslogAvailable = self.IsSyslogAvailable()
+
+    def IsSyslogAvailable(self):
+        try:
+            r = requests.get("http://" + self.ip + self.SYSLOG_URL, timeout=2)
+        except:
+            return False
+
+        if r.status_code == 200:
+            return True
+        else:
+            return False
 
     def IsTelemetryAvailable(self):
         try:
@@ -39,6 +52,52 @@ class MonitoringInformation():
             return True
         else:
             return False
+            
+    def ApplySyslogConfig(self, syslogCfg):
+        jsonCfg = {}
+        
+        jsonCfg["config"] = {}
+        jsonCfg["config"]["server"] = syslogCfg["syslog_target"]
+        jsonCfg["config"]["enable"] = True if "enable_syslog" in syslogCfg else False
+        
+        jsonCfg["monitoring"] = {}
+        jsonCfg["monitoring"]["common"] = {}
+        jsonCfg["monitoring"]["common"]["ptp_event"] = True if "ptp_event" in syslogCfg else False
+        jsonCfg["monitoring"]["common"]["temp_event"] = True if "temp_event" in syslogCfg else False
+        jsonCfg["monitoring"]["common"]["rtp_48k_event"] = True if "rtp_48k_event" in syslogCfg else False
+        jsonCfg["monitoring"]["common"]["fan_speed"] = True if "fan_speed" in syslogCfg else False
+        
+        jsonCfg["monitoring"]["encap"] = {}
+        jsonCfg["monitoring"]["encap"]["sdi_event"] = True if "sdi_event" in syslogCfg else False
+        jsonCfg["monitoring"]["encap"]["no_signal"] = True if "no_signal" in syslogCfg else False
+        
+        jsonCfg["monitoring"]["decap"] = {}
+        jsonCfg["monitoring"]["decap"]["output_flywheel"] = True if "output_flywheel" in syslogCfg else False
+        jsonCfg["monitoring"]["decap"]["memory_pkt_error"] = True if "memory_pkt_error" in syslogCfg else False
+        jsonCfg["monitoring"]["decap"]["flow_impairment"] = True if "flow_impairment" in syslogCfg else False
+        jsonCfg["monitoring"]["decap"]["frame_repeat"] = True if "frame_repeat" in syslogCfg else False
+        jsonCfg["monitoring"]["decap"]["frame_skipped"] = True if "frame_skipped" in syslogCfg else False
+        jsonCfg["monitoring"]["decap"]["dash7_fifo_error"] = True if "dash7_fifo_error" in syslogCfg else False
+        
+        app.logger.warning("Cfg: " + str(jsonCfg))
+        
+        self.set_config("http://" + self.ip + self.SYSLOG_URL, jsonCfg)
+
+    def set_config(self, url, json_config, ignore_error=False, timeout=5, retry_interval=1, retry_max=5):
+        app.logger.warning(str(url))
+        retry_count = 0
+        r = requests.put(url, json=json_config, timeout=timeout)
+
+        while retry_count < retry_max:
+            if r.status_code == 200:
+                return r.status_code
+            else:
+                retry_count += 1
+                if not ignore_error:
+                    print("PUT failed, retrying in a second...")
+                    time.sleep(retry_interval)
+            r = requests.put(url, json=json_config, timeout=timeout)
+        return r.status_code
 
 
 class PrometheusServer():
@@ -84,6 +143,12 @@ class SyslogEntry():
         except:
             app.logger.warning("Could not parse syslog: " + str(rawString))
 
+
+def ApplySyslogConfigToAllUnits(syslogCfg):
+    for device in monitored_devices:
+        if device.syslogAvailable:
+            device.ApplySyslogConfig(syslogCfg)
+
 def LoadConfig(configFile="config.json"):
     if os.path.exists(configFile):
         # Load Config...
@@ -93,23 +158,29 @@ def LoadConfig(configFile="config.json"):
     else:
         # Save Blank Config...
         app.logger.warning("Cannot load config, creating a new one based on defaults")
-        blank_config = {}
-        blank_config["syslog_target"] = "192.168.39.100"
-        blank_config["enable_syslog"] = True
-        blank_config["ptp_event"] = True
-        blank_config["temp_event"] = True
-        blank_config["rtp_48k_event"] = True
-        blank_config["fan_speed"] = True
-        blank_config["sdi_event"] = True
-        blank_config["no_signal"] = True
-        blank_config["output_flywheel"] = True
-        blank_config["memory_pkt_error"] = True
-        blank_config["flow_impairment"] = True
-        blank_config["frame_repeat"] = True
-        blank_config["frame_skipped"] = True
-        blank_config["dash7_fifo_error"] = True
-        
-        SaveConfig(blank_config)
+        data = GenerateDefaultSyslogConfig()
+        SaveConfig(data)
+        return data
+
+
+def GenerateDefaultSyslogConfig():
+    blank_config = {}
+    blank_config["syslog_target"] = "192.168.39.100"
+    blank_config["enable_syslog"] = True
+    blank_config["ptp_event"] = True
+    blank_config["temp_event"] = True
+    blank_config["rtp_48k_event"] = True
+    blank_config["fan_speed"] = True
+    blank_config["sdi_event"] = True
+    blank_config["no_signal"] = True
+    blank_config["output_flywheel"] = True
+    blank_config["memory_pkt_error"] = True
+    blank_config["flow_impairment"] = True
+    blank_config["frame_repeat"] = True
+    blank_config["frame_skipped"] = True
+    blank_config["dash7_fifo_error"] = True
+    return blank_config
+
 
 def GenerateConfigDataFromRequest(requestForm):
     save_data = {}
@@ -253,6 +324,9 @@ def MainPage():
             volumes={'grafana_to_monitor': {'bind': '/home/to_monitor/', 'mode': 'rw'}}, 
             publish_all_ports=True,
             network="grafana_bridge_net")
+            
+        newDev.ApplySyslogConfig(config)
+        
         return render_template('index.html', monitoredDevices=monitored_devices)
     elif request.method == 'POST' and "configure" in request.form:
         return show_configuration_page(config)
@@ -281,6 +355,7 @@ def MainPage():
         
     elif request.method == 'POST' and "saveConfig" in request.form: 
         SaveConfig(GenerateConfigDataFromRequest(request.form))
+        ApplySyslogConfigToAllUnits(request.form)
         return render_template('index.html')
 
     else:
@@ -290,6 +365,7 @@ def MainPage():
 
 
 if __name__ == '__main__':
+    config = LoadConfig()
     image_id = docker_client.images.get("prometheus_interface:latest").id
     for container in docker_client.containers.list():
         if container.image.id == image_id:
@@ -302,6 +378,7 @@ if __name__ == '__main__':
             if devIp is not None:
                 app.logger.warning("Adding an already present monitor container: " + container.name)
                 newDev = MonitoringInformation(devIp, container.name)
+                newDev.ApplySyslogConfig(config)
                 monitored_devices.append(newDev)
             else:
                 app.logger.warning("Could not add container: " + container.name)
